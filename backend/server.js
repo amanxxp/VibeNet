8915c2a61e59856b3459e83284a77390334792e3
Aman <aman@kali.kali>
2024-04-30 20:38:26 +0530

    3rd commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is runing on port ${PORT}`);
